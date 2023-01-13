11dde76470ea3a33962b6a618d4ab69b51850d2d
Mariama KaiKai <mmak.webdev@gmail.com>
2023-01-13 21:43:57 +0900

    new rails app created with routes seeds and controller files completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating a flat..."
+
+6.times do
+  flat = Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: 99,
+    number_of_guests: 3
+    )
+
+puts "Flat with id: #{flat.id} has been created"
+end
+
+puts "Finished!"
